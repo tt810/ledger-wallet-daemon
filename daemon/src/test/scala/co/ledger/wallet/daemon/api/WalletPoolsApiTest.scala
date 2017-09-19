eff9782fccb218c5da3e055ae9f4acbad3159524
Ting Tu <tingtu@Tings-MacBook-Pro.local>
2017-09-19 12:23:32 +0200

    Change models Pool to WalletPool

--- daemon/src/test/scala/co/ledger/wallet/daemon/api/WalletPoolsApiTest.scala
+++ daemon/src/test/scala/co/ledger/wallet/daemon/api/WalletPoolsApiTest.scala
@@ -1,7 +1,7 @@
 package co.ledger.wallet.daemon.api
 
 import co.ledger.wallet.daemon.{ErrorCode, ErrorResponseBody, models}
-import co.ledger.wallet.daemon.models.Pool
+import co.ledger.wallet.daemon.models.WalletPool
 import co.ledger.wallet.daemon.utils.APIFeatureTest
 import com.twitter.finagle.http.Status
 
@@ -9,27 +9,27 @@ class WalletPoolsApiTest extends APIFeatureTest {
 
   test("WalletPoolsApi#Create and list single pool") {
     createPool("my_pool")
-    val pools = parse[List[models.Pool]](getPools())
-    assert(pools == List(Pool("my_pool", 0)))
+    val pools = parse[List[models.WalletPool]](getPools())
+    assert(pools == List(WalletPool("my_pool", 0)))
     deletePool("my_pool")
   }
 
   test("WalletPoolsApi#Create and list multiple pool") {
     createPool("your_pool")
     createPool("this_pool")
-    val pools = parse[List[models.Pool]](getPools())
-    assert(pools == List(Pool("your_pool", 0), Pool("this_pool", 0)))
+    val pools = parse[List[models.WalletPool]](getPools())
+    assert(pools == List(WalletPool("your_pool", 0), WalletPool("this_pool", 0)))
     deletePool("your_pool")
     deletePool("this_pool")
-    val pools2 = parse[List[models.Pool]](getPools())
+    val pools2 = parse[List[models.WalletPool]](getPools())
     assert(pools2.size == 0)
   }
 
   test("WalletPoolsApi#Get single pool") {
     val response = createPool("anotha_pool")
-    assert(server.mapper.objectMapper.readValue[models.Pool](response.contentString) == Pool("anotha_pool", 0))
-    val pool = parse[models.Pool](getPool("anotha_pool"))
-    assert(pool == Pool("anotha_pool", 0))
+    assert(server.mapper.objectMapper.readValue[models.WalletPool](response.contentString) == WalletPool("anotha_pool", 0))
+    val pool = parse[models.WalletPool](getPool("anotha_pool"))
+    assert(pool == WalletPool("anotha_pool", 0))
     deletePool("anotha_pool")
   }
 
@@ -44,8 +44,8 @@ class WalletPoolsApiTest extends APIFeatureTest {
 
   test("WalletPoolsApi#Create same pool twice return ok") {
     assert(
-      server.mapper.objectMapper.readValue[models.Pool](createPool("same_pool").contentString)
-        == Pool("same_pool", 0))
+      server.mapper.objectMapper.readValue[models.WalletPool](createPool("same_pool").contentString)
+        == WalletPool("same_pool", 0))
     assert(
       server.mapper.objectMapper.readValue[ErrorResponseBody](createPool("same_pool").contentString)
         == ErrorResponseBody(ErrorCode.Duplicate_Request, "Attempt creating same_pool request is ignored"))
